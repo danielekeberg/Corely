dba867662db7d4e9b1dd295d3e6742d13830f114
Daniel Lehre Ekeberg <danieleke@live.no>
2025-09-25 13:48:29 +0200

    Update Section.tsx

--- src/app/Section.tsx
+++ src/app/Section.tsx
@@ -1,4 +1,4 @@
-function Section(prompt) {
+function Section(prompt: { title: string; desc: string }) {
     return (
         <div className="p-8 text-center">
             <h2 className="text-4xl font-semibold mb-4">{prompt.title}</h2>
