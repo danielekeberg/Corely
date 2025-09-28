3bc4bfc0148fc06796e86364576e17446239d59d
Daniel Lehre Ekeberg <154937462+danielekeberg@users.noreply.github.com>
2025-09-28 02:13:53 +0200

    Update useCart.tsx

--- src/app/useCart.tsx
+++ src/app/useCart.tsx
@@ -24,5 +24,24 @@ export function useCart() {
         setCart((prev) => prev.filter((item) => item.id !== id));
     }
 
-    return { cart, addToCart, removeFromCart };
+    function setQuantity(id: string | number, qty: number) {
+        setCart(prev => {
+            if(qty <= 0) return prev.filter(it => it.id !== id);
+            const i = prev.findIndex(it => it.id === id);
+            if(i === 1) return [...prev, { id, quantity: qty }];
+            const next = [...prev];
+            next[i] = { ...next[i], quantity: qty};
+            return next;
+        })
+    }
+
+    function increment(id: string | number) {
+        setQuantity(id, (cart.find(it => it.id === id)?.quantity ?? 0) + 1);
+    }
+
+    function decrement(id: string | number) {
+        setQuantity(id, (cart.find(it => it.id === id)?.quantity ?? 0) - 1);
+    }
+
+    return { cart, addToCart, removeFromCart, setQuantity, increment, decrement };
 }
