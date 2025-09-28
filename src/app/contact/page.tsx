ad1e07872728d5fdd2216e2a7850839c2f32c4d6
Daniel Lehre Ekeberg <154937462+danielekeberg@users.noreply.github.com>
2025-09-28 02:13:08 +0200

    Update contacgt/page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,13 +1,15 @@
 import Header from "../Header";
 import Section from "../Section";
-import Contact from "../Contact";
+// import Contact from "../Contact";
+import ContactForm from "../ContactForm";
 
 function App() {
     return (
         <div>
             <Header />
             <Section title="Contact" desc="Have a question or need help? We're here to assist you." />
-            <Contact />
+            {/* <Contact /> */}
+            <ContactForm />
         </div>
     )
 }
