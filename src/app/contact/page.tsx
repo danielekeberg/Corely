501e832253c56170ff81397a7fd67b1e2db0de26
Daniel Lehre Ekeberg <danieleke@live.no>
2025-09-28 02:59:30 +0200

    Update contact/page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,5 @@
 import Header from "../Header";
 import Section from "../Section";
-// import Contact from "../Contact";
 import ContactForm from "../ContactForm";
 
 function App() {
@@ -8,7 +7,6 @@ function App() {
         <div>
             <Header />
             <Section title="Contact" desc="Have a question or need help? We're here to assist you." />
-            {/* <Contact /> */}
             <ContactForm />
         </div>
     )
